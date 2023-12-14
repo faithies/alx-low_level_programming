846f0b4684de638a80ffe32958366904091d25a5
faithies <jeff20474@gmail.com>
2023-12-14 11:14:38 -0800

    a program that prints to stdout

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - entry point of code
+ * puts: printf output
+ * Return: Always 0 on success
+ */
 
-int main()
+
+int main(void)
 {
-puts ("\"programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
