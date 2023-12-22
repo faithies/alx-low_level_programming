7bc96e7564fa2ea4d5e28639379b907af1b38928
faithies <jeff20474@gmail.com>
2023-12-22 09:35:33 -0800

    a function to check for negative or positive number

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 on success
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
