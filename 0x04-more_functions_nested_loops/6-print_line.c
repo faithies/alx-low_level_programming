ab8ba50ae1461246265a086aac40f3a8d72e7cba
faithies <jeff20474@gmail.com>
2023-12-26 00:40:59 -0800

    a function that prints a striaght line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_line - function name
@@ -12,12 +13,12 @@ void print_line(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (n <= 0)
+		if ((n < 0) || (n == 0))
 		{
 			_putchar('\n');
 		}
 
 		_putchar('-');
 	}
-	_putchar('\n');
+_putchar('\n');
 }
