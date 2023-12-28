a04efc5a2ce60ed3dda9ad22c2e7354a6cae4ed9
faithies <jeff20474@gmail.com>
2023-12-28 08:58:19 -0800

    a program that prints to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point of code
+ * Return: Always 1 on failure
+ */
 
 int main(void)
 {
 
-	fwrite("and that piece of art is useful\" - \ Dora Korpar, 2015-10-19\n");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
