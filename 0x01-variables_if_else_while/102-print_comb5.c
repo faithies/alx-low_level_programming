0bd116a7dc980df8855bfd5dccc69413ae74fa17
faithies <jeff20474@gmail.com>
2023-12-30 05:58:57 -0800

    a program that prints all pissible combinations of two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - function name
+ * @Return: Always 0 on success
+ */
+
+int main(void)
+{
+	int first_num;
+	int second_num;
+
+	for (first_num = 0; first_num < 100; first_num++)
+	{
+		for (second_num = 0; second_num < 100; second_num++)
+		{
+			if (first_num < second_num)
+			{
+				putchar('0' + (first_num / 10));
+				putchar('0' + (first_num % 10));
+				putchar(' ');
+				putchar('0' + (second_num / 10));
+				putchar('0' + (second_num % 10));	
+
+				if (second_num != 99 || first_num != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
