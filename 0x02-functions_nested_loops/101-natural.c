cda72d1a6336b3b5e022c593c91e8addfed5145c
faithies <jeff20474@gmail.com>
2024-01-01 12:19:47 -0800

    a program that computes and prints the sun of the multiples of 3 or 5 below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,10 +13,9 @@ int main(void)
 
 	for (index = 0; index < 1024; index++)
 	{
-		sum = sum + index;
-
 		if (index % 3 == 0 || index % 5 == 0)
 		{
+			sum = sum + index;
 			printf("%d", sum);
 		}
 		else
