f3182984e9c2a660276cdf86f371d63f6bc028b6
faithies <jeff2074@gmail.com>
2024-01-15 09:52:56 -0800

    a function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _strpbrk - function name
+ * @s: function string to check
+ * @accept: character whose occurance is to be checked
+ * Return: Always pointer to s on success and NULL if no character is found
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
