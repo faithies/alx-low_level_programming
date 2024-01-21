8dc97d20d512d2f7fdb4a402401602858e7daf05
faithies <jeff2074@gmail.com>
2024-01-21 04:58:46 -0800

    a function that returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,30 +7,29 @@
  * @height: function parameter and diamension of a 2d array
  * Return: pointer to th newly allocated space in memory on success
  */
-
 int **alloc_grid(int width, int height)
 {
 	int iterator1;
 	int iterator2;
-	int **ptr;/*pointer to pointer */
+	int **ptr;
 
-	if ((width <= 0) || (height <= 0))/*checks for invalid input*/
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	ptr = (int **) malloc(sizeof(int **) * height);/*memory for rows (height)*/
-	if (ptr == NULL)/*on allocation failure*/
+	ptr = (int **)malloc(sizeof(int *) * height);
+
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 
 	for (iterator1 = 0; iterator1 < height; iterator1++)
-
 	{
-		ptr[iterator1] = malloc(sizeof(int *) * width);
+		ptr[iterator1] = (int *)malloc(sizeof(int) * width);
 
-		if (ptr[iterator1] == NULL)/*free previously allocated memory on failure*/
+		if (ptr[iterator1] == NULL)
 		{
 			for (iterator2 = 0; iterator2 < iterator1; iterator2++)
 			{
@@ -42,11 +41,8 @@ int **alloc_grid(int width, int height)
 
 		for (iterator2 = 0; iterator2 < width; iterator2++)
 		{
-			ptr[iterator1][iterator2] = 0;/*initialize each elment with 0*/
+			ptr[iterator1][iterator2] = 0;
 		}
 	}
-
-
-
 	return (ptr);
 }
