5adf9acec7cf22ab364deb4c0d5c625b82a1f4f9
faithies <jeff2074@gmail.com>
2024-01-23 02:24:04 -0800

    a struct with elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - declaration that defines a physically
+ * grouped list of variables under one name in a block
+ * of memory
+ *
+ * @name: pointer to characters in memory
+ * @age: member of the struct that stores the age in memory
+ * @owner: pointer to character in memory
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
