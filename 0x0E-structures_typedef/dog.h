445f223fc0ffefbfee9eea19be1cab3365c4a0b8
faithies <jeff2074@gmail.com>
2024-01-23 17:01:55 -0800

    a new struct type defination as a new name of a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,13 +10,12 @@
  * @age: member of the struct that stores the age in memory
  * @owner: pointer to character in memory
  */
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
