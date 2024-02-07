d622b92c8bad962caec79115c0b67029dd169cf0
faithies <jeff2074@gmail.com>
2024-02-06 16:29:40 -0800

    a function that prints all the elements of a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,14 +15,14 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 
-		if (h->str)
+		if (!h->str)
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[0](nil)\n");
 
 		}
 		else
 		{
-			printf("[0](nil)\n");
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 
