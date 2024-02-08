aba6ee28a2a39edb31dc8913a090fedf48c9ba07
faithies <jeff2074@gmail.com>
2024-02-08 04:12:06 -0800

    a function that adds a new node to the end of a linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,24 +1,43 @@
 #include "lists.h"
 
+/**
+ * add_node_end - function name that adds a new at the end
+ * of a linked list
+ * @head: pointer to the first element of the linked list
+ * @str: pointer to the duplicated string
+ * Return: address of teh new node
+ */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new;
+	list_t *temp;
+	unsigned int len = 0;
 
-unsigned int temp;
-unsigned int len = 0;
+	temp = *head;
 
-	new = malloc(sizeof(list_t));
-	
-	if(new == NULL)
-	{
-		retrun (NULL);
-	}
+	while (str[len])
 
-
-	while(str[len])
-	{
 		len++;
 
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
 	}
 
-	return(*new);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+	return (new);
 }
